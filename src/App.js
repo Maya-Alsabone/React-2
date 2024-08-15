64707a4c849da1c6f66e8532672a53ac8c1aaea2
Maya Alsabone <mayaalsabone@gmail.com>
2024-08-15 13:06:21 +0100

    Avatar css and js added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import './App.css'; // Importing the CSS file
+
 function MyButton() {
   return (
     <button>
@@ -15,13 +18,14 @@ function AboutPage() {
   );
 }
 
+
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
       <MyButton />
       <AboutPage />
-      <img className="avatar" src="path-to-your-image.jpg" alt="Avatar" />
+      <img className="avatar" src="https://static-00.iconduck.com/assets.00/user-avatar-icon-2048x2048-wpp8os2d.png" alt="Avatar" />
     </div>
   );
 }
